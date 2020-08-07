00e222f11eca3eae206c7c22e687caea59cb88ea
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-08-08 00:34:46 +0900

    feat (linked-list): add test INSPECTION

--- src/data-structure/sequences/linked-list/__tests__/linked-list.test.ts
+++ src/data-structure/sequences/linked-list/__tests__/linked-list.test.ts
@@ -1,8 +1,18 @@
 import LinkedList from "../linked-list";
 
-test("init LinkedList", () => {
+test("LinkedList INSPECTION", () => {
+  // LinkedList インスタンス作成
   const list = new LinkedList<string>();
+  // 空のときのisEmpty
   expect(list.isEmpty()).toBe(true);
+  // 空のときのsize
+  expect(list.size()).toBe(0);
+  // 追加済みのisEmpty
+  list.addFront("one");
+  // 追加済みのisEmpty
+  expect(list.isEmpty()).toBe(false);
+  // 追加済みのsize
+  expect(list.size()).toBe(1);
 });
 test("addFront & addBack", () => {
   const list = new LinkedList<string>();
