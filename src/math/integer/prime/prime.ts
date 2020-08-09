4dc197f6615863eb1995a9b702d7fc11ab37394c
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-08-09 19:05:55 +0900

    feat (prime): add prime-fast

--- src/math/integer/prime/prime.ts
+++ src/math/integer/prime/prime.ts
@@ -14,3 +14,25 @@ export function isPrimeNormal(x: number): boolean {
   }
   return true;
 }
+
+/**
+ * PrimeCheckFunction use composite number - O(√n)
+ * @param {number} x - the number for isPrime-check
+ * @return {boolean}
+ */
+export function isPrime(x: number): boolean {
+  if (x === 2) {
+    return true;
+  }
+  // 2以外の偶数は素数でないことを利用し高速化
+  if (x < 2 || x % 2 === 0) {
+    return false;
+  }
+  // 合成数xは p<=√xを満たす素因子pを持つを利用し調べる範囲を最小化
+  for (let i = 3; i < Math.sqrt(x); i += 2) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
