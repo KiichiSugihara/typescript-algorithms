e09b9639d8b08af485e5747a0db3ba0f8591047b
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-10-03 14:40:22 +0900

    add getDaysFromNewYearDay year-test

--- src/algorithms/getDaysFromNewYear'sDay/__tests__/getDaysFromNewYear'sDay.test.ts
+++ src/algorithms/getDaysFromNewYear'sDay/__tests__/getDaysFromNewYear'sDay.test.ts
@@ -1,6 +1,15 @@
 import { getDaysFromNewYearDay } from "../getDaysFromNewYear'sDay";
 
 test("CHECK getDaysFromNewYearDay", () => {
+  // YEAR
   // 4の倍数でないもの
-  expect(getDaysFromNewYearDay(2003, 12, 31)).toBe(0);
+  expect(getDaysFromNewYearDay(2003, 2, 29)).toBe(0);
+  // 4の倍数
+  expect(getDaysFromNewYearDay(2004, 2, 29)).toBe(0);
+  // 100の倍数で400の倍数でない数
+  expect(getDaysFromNewYearDay(2100, 2, 29)).toBe(0);
+  // 100の倍数で400の倍数
+  expect(getDaysFromNewYearDay(2400, 2, 29)).toBe(0);
+  // MONTH
+  // DAY
 });
--- src/algorithms/getDaysFromNewYear'sDay/getDaysFromNewYear'sDay.ts
+++ src/algorithms/getDaysFromNewYear'sDay/getDaysFromNewYear'sDay.ts
@@ -1,3 +1,4 @@
+import { checkLeapYear } from "./checkLeapYear";
 /**
  * getDaysFromNewYear'sDay - O(?)
  * @param {year,month,day} x - the number for isPrime-check
@@ -10,8 +11,9 @@ export function getDaysFromNewYearDay(
 ): number {
   const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   // check leap year
-  // if (checkLeapYear(year)) {
-  // }
+  if (checkLeapYear(year)) {
+    days[1] = 29;
+  }
   console.log(year, month, day, days);
   const countDay = 0;
   return countDay;
