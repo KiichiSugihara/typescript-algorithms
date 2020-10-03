65fa92265279a8aaa334d27b55f139c10ce2fa79
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-10-03 15:02:50 +0900

    add getDaysFromNewYearDay month-test

--- src/algorithms/getDaysFromNewYear'sDay/__tests__/getDaysFromNewYear'sDay.test.ts
+++ src/algorithms/getDaysFromNewYear'sDay/__tests__/getDaysFromNewYear'sDay.test.ts
@@ -1,7 +1,6 @@
 import { getDaysFromNewYearDay } from "../getDaysFromNewYear'sDay";
 
-test("CHECK getDaysFromNewYearDay", () => {
-  // YEAR
+test("CHECK YEAR", () => {
   // 4の倍数でないもの
   expect(getDaysFromNewYearDay(2003, 2, 29)).toBe(0);
   // 4の倍数
@@ -10,6 +9,31 @@ test("CHECK getDaysFromNewYearDay", () => {
   expect(getDaysFromNewYearDay(2100, 2, 29)).toBe(0);
   // 100の倍数で400の倍数
   expect(getDaysFromNewYearDay(2400, 2, 29)).toBe(0);
-  // MONTH
-  // DAY
+});
+
+test("CHECK MONTH", () => {
+  // 1月
+  expect(getDaysFromNewYearDay(2004, 1, 15)).toBe(0);
+  // 2月
+  expect(getDaysFromNewYearDay(2004, 2, 15)).toBe(0);
+  // 3月
+  expect(getDaysFromNewYearDay(2004, 3, 15)).toBe(0);
+  // 4月
+  expect(getDaysFromNewYearDay(2004, 4, 15)).toBe(0);
+  // 5月
+  expect(getDaysFromNewYearDay(2004, 5, 15)).toBe(0);
+  // 6月
+  expect(getDaysFromNewYearDay(2004, 6, 15)).toBe(0);
+  // 7月
+  expect(getDaysFromNewYearDay(2004, 7, 15)).toBe(0);
+  // 8月
+  expect(getDaysFromNewYearDay(2004, 8, 15)).toBe(0);
+  // 9月
+  expect(getDaysFromNewYearDay(2004, 9, 15)).toBe(0);
+  // 10月
+  expect(getDaysFromNewYearDay(2004, 10, 15)).toBe(0);
+  // 11月
+  expect(getDaysFromNewYearDay(2004, 11, 15)).toBe(0);
+  // 12月
+  expect(getDaysFromNewYearDay(2004, 12, 15)).toBe(0);
 });
--- src/algorithms/getDaysFromNewYear'sDay/getDaysFromNewYear'sDay.ts
+++ src/algorithms/getDaysFromNewYear'sDay/getDaysFromNewYear'sDay.ts
@@ -10,11 +10,24 @@ export function getDaysFromNewYearDay(
   day: number
 ): number {
   const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let countDay = 0;
+  // year,month ,dayのエラー処理
+  // year 1以上
+  // month 1~12
+  // day 該当月の所有日を超えているか？
+  // day うるう年でない際の2/29エラー
+
   // check leap year
   if (checkLeapYear(year)) {
     days[1] = 29;
   }
-  console.log(year, month, day, days);
-  const countDay = 0;
-  return countDay;
+  // 2月以降なら超えている月分カウンター足す
+  if (month > 1) {
+    for (let i = 0; i < month - 1; i++) {
+      countDay += days[i];
+    }
+  }
+  console.log(year, month, day, days, countDay);
+
+  return 0;
 }
