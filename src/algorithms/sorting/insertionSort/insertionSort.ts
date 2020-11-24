a969ca85bb897b04757e87d03c4e18ebca00b267
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-11-24 12:00:30 +0900

    feat (insertionSort): add algo&test

--- src/algorithms/sorting/insertionSort/insertionSort.ts
+++ src/algorithms/sorting/insertionSort/insertionSort.ts
@@ -1,11 +1,21 @@
 /**
- * Searches for a target element within an array.
- * Returns -1 if the target is not found.
+ * Sort Array
+ * Returns sorted Array
  * @param {number[]} arr
- * @param {number} target
- * @return {number} The index of the target element within the array.
+ * @return {number[]}
  */
-export function insertionSort(arr: Array<number>, target: number): number {
-  console.log(arr, target);
-  return target;
+export function insertionSort(arr: Array<number>): Array<number> {
+  for (let i = 1; i < arr.length; i++) {
+    const tmp = arr[i];
+    if (arr[i - 1] > tmp) {
+      let j = i;
+
+      while (j > 0 && arr[j - 1] > tmp) {
+        arr[j] = arr[j - 1];
+        j--;
+      }
+      arr[j] = tmp;
+    }
+  }
+  return arr;
 }
