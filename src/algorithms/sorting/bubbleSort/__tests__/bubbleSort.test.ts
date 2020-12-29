0d99db82c8bb6516b108ca1481dd7b99bafdf89b
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-12-29 19:55:59 +0900

    chore(bubbleSort):delete uncomplete

--- src/algorithms/sorting/bubbleSort/__tests__/bubbleSort.test.ts
+++
@@ -1,7 +0,0 @@
-import { bubbleSort } from "../bubbleSort";
-
-describe("bubbleSort", () => {
-  it("should sort number in sorted array", () => {
-    expect(bubbleSort([], 1)).toBe(1);
-  });
-});
--- src/algorithms/sorting/bubbleSort/index.ts
+++
@@ -1,1 +0,0 @@
-export { bubbleSort } from "./bubbleSort";
