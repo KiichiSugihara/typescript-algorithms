780541ad46113b9658995ed8c924437d2d90bc6e
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-02 21:43:49 +0900

    feat:add template

---
+++ src/other/template/template.ts
@@ -0,0 +1,11 @@
+/**
+ * TemplateFunction  - O(1)
+ * @param {number} x - the number for is0check
+ * @return {boolean}
+ */
+export function templateIs0(x: number): boolean {
+  if (x === 0) {
+    return true;
+  }
+  return false;
+}
