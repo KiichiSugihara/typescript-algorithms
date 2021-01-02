51fece2a2402c1edaf3928e86d3cc4a4948a6e17
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-02 23:07:40 +0900

    feat(tribonacci):finish pre-tribonacciMemo

--- src/math/number/trbionacci/tribonacciMemo.ts
+++ src/math/number/trbionacci/tribonacciMemo.ts
@@ -1,13 +1,25 @@
-/**
- * TribonacciRecursion
- * @param {number} x - the number order for tribonacci
- * @return {number} - Tribonacci number
- */
-export function tribonacciRecursion(n: number): number {
+// /**
+//  * TribonacciMemo
+//  * @param {number} x - the number order for tribonacci
+//  * @return {number} - Tribonacci number
+//  */
+export const tribonacciMemo = memorize(slowTri);
+
+interface slowTriType {
+  (n: number): number;
+}
+
+function slowTri(n: number): number {
   if (n < 3) return n === 2 ? 1 : 0;
-  return (
-    tribonacciRecursion(n - 3) +
-    tribonacciRecursion(n - 2) +
-    tribonacciRecursion(n - 1)
-  );
+  return tribonacciMemo(n - 3) + tribonacciMemo(n - 2) + tribonacciMemo(n - 1);
+}
+
+function memorize(fn: slowTriType) {
+  const cache: Array<number> = [];
+  return (args: number) => {
+    if (cache[args]) return cache[args];
+    const result = fn(args);
+    cache[args] = result;
+    return result;
+  };
 }
